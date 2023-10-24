9ca43af470f7949724891c8ad34f6a11ef97360a
Kent Erik <kent.erik.hole@outlook.com>
2023-10-24 17:57:47 +0200

    fix the

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './script/app'
+import App from './script/App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
