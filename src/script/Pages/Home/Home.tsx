1179978918bbd3004ce970234aae211d2f6e9a38
Kent Erik <kent.erik.hole@outlook.com>
2023-10-24 15:58:31 +0200

    testing out loading and Error msg

--- src/script/Pages/Home/Home.tsx
+++ src/script/Pages/Home/Home.tsx
@@ -1,4 +1,25 @@
+import { useState } from "react"
+
+import Loading from "../../Layout/Loading/Loading"
+import Error from "../../Layout/Error/Error"
+
 export default function Home() {
+    const [isLoading, SetIsLoading] = useState(true)
+    const [isError, SetIsError] = useState(true)
+
+    
+
+    if(isError){
+        return(
+            <Error />
+        )
+    }
+
+    if(isLoading){
+        return(
+            <Loading />
+        )
+    } 
     return(
         <h1>test</h1>
     )
