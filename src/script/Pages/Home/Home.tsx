2cf27bf274d283d6c14d5ff7230e2f9d6756e922
Kent Erik <kent.erik.hole@outlook.com>
2023-10-26 14:56:42 +0200

    adding home

--- src/script/Pages/Home/Home.tsx
+++ src/script/Pages/Home/Home.tsx
@@ -4,8 +4,10 @@ import Error from "../../Layout/Error/Error"
 import { ApiRequeset } from "../../Components/API/ApiRequeset";
 import Header from "../../Layout/Header/Header";
 
+
 export default function Home() {
     const {data, isLoading, isError} = ApiRequeset()
+    
 
     if(isError){
         return(
