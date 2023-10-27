5ee23d84c6252ffc63a02f662fca08dde996b753
Kent Erik <kent.erik.hole@outlook.com>
2023-10-27 18:33:52 +0200

    adding hooking form

--- src/script/Pages/Register/RegisterConfig.tsx
+++ src/script/Pages/Register/RegisterConfig.tsx
@@ -1,7 +1,11 @@
+import RegisterForm from "../../Components/Form/RegisterForm"
+
+
 export default function RegisterConfig () {
     return (
-        <div>
-            test
-        </div>
+        <main className="m-3 p-3">
+            <RegisterForm/>
+            
+        </main>
     )
 }
