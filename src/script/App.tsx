424a9dd2faf6ed2910d4a48ba8e4a38e6e19c53b
Kent Erik <kent.erik.hole@outlook.com>
2023-10-27 16:07:08 +0200

    adding Register PAge Config file

--- src/script/App.tsx
+++ src/script/App.tsx
@@ -1,9 +1,17 @@
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import '../css/style.css';
+// Import 
+    // Import React 
+        import { BrowserRouter,  Routes, Route } from "react-router-dom";
+    // Import Css 
+        import '../css/style.css';
+    // Layout
+        import Layout from "./Layout/Layout";
 
-import Layout from "./Layout/Layout";
-import Home from "./Pages/Home/Home";
+    // Page Config 
+        // Home 
+            import Home from "./Pages/Home/Home";
+        // Register User 
+            import RegisterConfig from "./Pages/Register/RegisterConfig";
 
 export default function App(){
     return (
@@ -11,6 +19,7 @@ export default function App(){
             <Routes>
                 <Route path="/" element={<Layout></Layout>}>
                     <Route index element={<Home/>}></Route>
+                    <Route path="/Register" element={<RegisterConfig></RegisterConfig>}></Route>
                 </Route>
             </Routes>
         </BrowserRouter>
