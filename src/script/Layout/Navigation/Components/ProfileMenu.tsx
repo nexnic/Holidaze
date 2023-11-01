c8ca0d92e40c871f903df73cf95ea585252a758e
Kent Erik <kent.erik.hole@outlook.com>
2023-11-01 18:25:54 +0100

    working changing the buttons

--- src/script/Layout/Navigation/Components/ProfileMenu.tsx
+++ src/script/Layout/Navigation/Components/ProfileMenu.tsx
@@ -7,6 +7,7 @@ import ProfileHandler from "../../../Handler/ProfileHandler"
 export default function ProfileMenu () {
     // Get Profile Image
     const Image = ProfileImage()
+    
 
 
 
@@ -18,12 +19,11 @@ export default function ProfileMenu () {
             </div>
                   
             <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Action</a></li>
                 <li>
-                    <button onClick={() => ProfileHandler()}>Profile</button>
+                    <button className="dropdown-item" onClick={()=> ProfileHandler}>Profile</button>
                 </li>
                 <li>
-                    <button onClick={(event) =>  LogoutHandler(event)}>Logout</button>
+                    <button className="dropdown-item" type="button" onClick={()=> LogoutHandler}>Logout</button>
                 </li>
             </ul>
     </div> 
