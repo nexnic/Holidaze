b05eefdfddee293f389016f21d960f24e4ce2137
Kent Erik <kent.erik.hole@outlook.com>
2023-11-02 17:31:52 +0100

    fix The bugg on Link for User Profile

--- src/script/Layout/Navigation/Components/ProfileMenu.tsx
+++ src/script/Layout/Navigation/Components/ProfileMenu.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from "react-router-dom"
 import ProfileImage from "../../../Components/Image/ProfileImage"
 import LogoutHandler from "../../../Handler/LogoutHandler"
-import ProfileHandler from "../../../Handler/ProfileHandler"
+import GetName from "../../../Components/GetInfo/GetName"
+// Test Remove This function for adding direct to the Profile Handler
+//import ProfileHandler from "../../../Handler/ProfileHandler"
 
 
 
 export default function ProfileMenu () {
+    const navigate = useNavigate()
     // Get Profile Image
     const Image = ProfileImage()
-    
+    const userID = GetName()
+
+    function HandlerProfile(){
+        console.log('click')
+        console.log(userID)
+        navigate(`/profile/${userID}`)
+    }
+
+
 
 
 
@@ -20,10 +32,10 @@ export default function ProfileMenu () {
                   
             <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
                 <li>
-                    <button className="dropdown-item" onClick={()=> ProfileHandler}>Profile</button>
+                    <button className="dropdown-item" onClick={HandlerProfile}>Profile</button>
                 </li>
                 <li>
-                    <button className="dropdown-item" type="button" onClick={()=> LogoutHandler}>Logout</button>
+                    <button className="dropdown-item" type="button" onClick={()=> LogoutHandler()}>Logout</button>
                 </li>
             </ul>
     </div> 
